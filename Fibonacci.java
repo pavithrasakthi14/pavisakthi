0f3bde37e511067cf76123b148056d9b7bb5aac7
pavithrasakthi14 <pavithrasakthivel1311@gmail.com>
2017-08-16 09:17:36 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main(String args[])throws IOException
+{
+Scanner sc=new Scanner(System.in);
+int n=0,n2=1,n3;
+int i,count=0;
+System.out.println(n1+" "+n2);
+for(i=2;i<=count;++1)
+{
+n3=n1+n2;
+n1=n2;
+n2=n3;
+System.out.println(" "+n3);
+}
+}
+}
